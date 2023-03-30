b02db981440253b97ceddbe49bf03033c0016dd9
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-03-29 17:17:01 -0700

    Create LandingPage component

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Search from './Search';
 import ProductList from './ProductList';
 import { Product } from '../../../types/product';
 
-const LandingPage = ({}) => {
+interface ILandingPageProps {
+    filteredProducts: Product[];
+    isLoading: boolean;
+    alert: { message: string; type: string };
+    setAlert: any;
+    searchRole: string;
+    setSearchTerm: any;
+    setSearchRole: any;
+    loadProductsError: any;
+}
+
+const LandingPage = ({
+    filteredProducts,
+    isLoading,
+    alert,
+    setAlert,
+    searchRole,
+    setSearchTerm,
+    setSearchRole,
+    loadProductsError,
+}: ILandingPageProps) => {
+    useEffect(() => {
+        if (loadProductsError)
+            setAlert({
+                message: 'There was an error loading the list of products.',
+                type: 'error',
+            });
+    }, [loadProductsError, setAlert]);
+
     return (
         <div id="wrapper" className="p-4">
             <header className="font-bold flex justify-center p-2">
                 milos-despotovic-is24-full-stack-competition-req97073
             </header>
             <main>
-                <Dashboard />
-                <Search />
-                <ProductList />
+                <Dashboard
+                    totalProducts={filteredProducts?.length}
+                    alert={alert}
+                    setAlert={setAlert}
+                />
+                <Search
+                    setSearchTerm={setSearchTerm}
+                    searchRole={searchRole}
+                    setSearchRole={setSearchRole}
+                />
+                <ProductList products={{ data: filteredProducts, isLoading }} />
+                <Outlet />
             </main>
         </div>
     );
