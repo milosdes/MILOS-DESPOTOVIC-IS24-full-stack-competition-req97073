a13a5e65f7fa2438f77a29a832a7ea201c567959
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-03-29 17:16:48 -0700

    Create Search component

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,7 +1,68 @@
 import React, { useState } from 'react';
 
-const Search = () => {
-    return <div>Search Component</div>;
+interface ISearchProps {
+    setSearchTerm: any;
+    searchRole: string;
+    setSearchRole: any;
+}
+
+const Search = ({ setSearchTerm, searchRole, setSearchRole }: ISearchProps) => {
+    const [value, setValue] = useState('');
+
+    //Styles for tailwind
+    const styles = {
+        button: 'border-2 rounded border-blue-200 text-blue-900 leading-normal hover:bg-blue-200 uppercase px-2 text-xs font-medium transition duration-150 ease-in-out',
+        developerActive: searchRole === 'Developers' ? 'bg-blue-200' : '',
+        scrumMasterActive: searchRole === 'Scrummaster' ? 'bg-blue-200' : '',
+    };
+
+    return (
+        <div className="m-6">
+            <div className="flex justify-center">
+                <label htmlFor="search" className="px-2 font-medium">
+                    Search
+                </label>
+            </div>
+            {/* Search input */}
+            <div className="flex justify-center">
+                <input
+                    className="border-2 rounded focus:outline-blue-200 m-2"
+                    name="search"
+                    value={value}
+                    onChange={(e) => {
+                        setValue(e.target.value);
+                        setSearchTerm(e.target.value);
+                    }}
+                />
+            </div>
+            {/* Search role buttons */}
+            <div className="flex justify-center">
+                <button
+                    type="button"
+                    className={`${styles.button} ${styles.developerActive}`}
+                    onClick={() => setSearchRole('Developers')}
+                >
+                    Developers
+                </button>
+                <button
+                    className={`${styles.button} ${styles.scrumMasterActive}`}
+                    onClick={() => setSearchRole('Scrummaster')}
+                >
+                    Scrum Master
+                </button>
+                <button
+                    className={`${styles.button}`}
+                    onClick={() => {
+                        setValue('');
+                        setSearchTerm('');
+                        setSearchRole('All');
+                    }}
+                >
+                    Clear
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Search;
