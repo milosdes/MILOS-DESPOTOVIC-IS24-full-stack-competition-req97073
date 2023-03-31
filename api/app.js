58277d88bdc08c67521660d73d290c986604094a
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-03-31 14:53:07 -0700

    Reorder api routes

--- api/app.js
+++ api/app.js
@@ -27,16 +27,16 @@ app.use('/api/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsonOptions));
 //Serve the build of the client app as a static resource
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-//Serve the index page for the client app
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
 //Use product endpoint routes
 app.use('/api/products', require('./routes/product.js'));
 
 //Check health of in-memory data object
 app.get('/api/health', productController.healthCheck);
+
+//Serve the index page for the client app
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 //Save updated data to data.json on exit
 process.on('exit', productController.saveDataOnExit);
